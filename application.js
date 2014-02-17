30134dc37c11a75e9716c66467e646c53b4de241
lewisr89 <lewisr89@gmail.com>
2014-02-17 13:24:30 -0500

    First Round

---
+++ application.js
@@ -0,0 +1,56 @@
+	
+$(document).ready(function() {
+
+	//Generate a random number between 1 and 100
+	var randomNumber = Math.floor(Math.random() * 101);
+    console.log(randomNumber);
+
+
+        $('#submitButton').click(function(){
+          var input = $('#guess').val();
+          var guess= Math.abs(input);
+          
+
+          if ((input == '') || (isNaN(input)) || (input < 1) || (input > 100)) { 
+                $('#message').html("Please put numbers 1-100");
+              }
+
+          else if (guess == randomNumber){
+                $("#message").html("You guessed it right!!").effect("pulsate", {times:4}, 2000);;
+                $("#congrats").show(1000);
+              }
+
+          else if (guess < (randomNumber+10) && guess > (randomNumber-10)){
+                $("#message").html("Really Hot!");
+              }
+
+          else if (guess < (randomNumber+20) && guess> (randomNumber-20)){
+                $("#message").html("Hot");
+              }
+
+          else {
+                $("#message").html("Cold");
+              }
+
+              $('#prevGuess').append(input).append(', ');
+              $('#guess').val('');
+         
+          
+          
+
+	});
+
+
+$('#newGame').click(function(){
+    randomNumber = Math.floor(Math.random() * 101);
+    console.log(randomNumber);
+    $("#congrats").hide();
+    $('#message').html('');
+    $('#prevGuess').html('');
+    $('#guess').val('');
+  });
+
+
+});
+
+	
