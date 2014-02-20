3d3d1f6205001a9a77b8a7e79c06ecda50b29596
lewisr89 <lewisr89@gmail.com>
2014-02-20 14:32:12 -0500

    changes after mentor meeting

--- application.js
+++ application.js
@@ -4,16 +4,21 @@ $(document).ready(function() {
 	//Generate a random number between 1 and 100
 	var randomNumber = Math.floor(Math.random() * 101);
     console.log(randomNumber);
-
-
-        $('#submitButton').click(function(){
+    
+    $('#submitButton').click(function(){
           var input = $('#guess').val();
           var guess= Math.abs(input);
           
+    
 
-          if ((input == '') || (isNaN(input)) || (input < 1) || (input > 100)) { 
-                $('#message').html("Please put numbers 1-100");
-              }
+          if ((guess == '') || (isNaN(guess)) || (guess < 1) || (guess > 100)) { 
+                $('#message').html("Please put in a number bewteen 1 and 100");
+
+              //Reset Text Box
+              $('#message').focus();
+              //Return back to user to make proper guess!
+              return;
+            }
 
           else if (guess == randomNumber){
                 $("#message").html("You guessed it right!!").effect("pulsate", {times:4}, 2000);;
@@ -35,11 +40,17 @@ $(document).ready(function() {
               $('#prevGuess').append(input).append(', ');
               $('#guess').val('');
          
-          
-          
+          //onkeydown="if (event.keyCode == 13) document.getElementById('submitButton').click()"
 
 	});
 
+    $('#guess').keypress(function (event) {
+        if (event.keyCode == 13) {
+            $("#submitButton").click();
+           
+            return false;
+        } 
+    });
 
 $('#newGame').click(function(){
     randomNumber = Math.floor(Math.random() * 101);
